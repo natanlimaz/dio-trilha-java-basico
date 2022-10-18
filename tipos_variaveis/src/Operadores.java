3a40b6f868e32fdffdef15dcbb562f12cfbc6049
natanlimaz <natanlimadev@gmail.com>
2022-10-18 12:06:55 -0300

    Classe Operadores

---
+++ tipos_variaveis/src/Operadores.java
@@ -0,0 +1,33 @@
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		/*
+		String concatenacao = "?";
+		
+		concatenacao = 1+1+1+"1";
+		System.out.println(concatenacao);
+		
+		concatenacao = 1+"1"+1+1;
+		System.out.println(concatenacao);
+		
+		concatenacao = 1+"1"+1+"1";
+		System.out.println(concatenacao);
+		
+		concatenacao = "1"+1+1+1;
+		System.out.println(concatenacao);
+		
+		concatenacao = "1"+(1+1+1);
+		System.out.println(concatenacao);
+		*/
+		
+		// Operadores unários
+		int numero = 5;
+		numero = -numero;
+		System.out.println(numero);
+		
+		numero = numero * -1;
+		System.out.println(numero);
+	}
+
+}
