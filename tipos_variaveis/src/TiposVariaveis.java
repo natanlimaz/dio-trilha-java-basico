ea44ef801a0c1441857cd68c02e8f9b5e98ad429
natanlimaz <natanlimadev@gmail.com>
2022-10-18 11:09:13 -0300

    Especificando tipos de dados

--- tipos_variaveis/src/TiposVariaveis.java
+++ tipos_variaveis/src/TiposVariaveis.java
@@ -2,9 +2,44 @@
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-	
+	/*
+		// int, byte, short, long, float, double, boolean, char
 		
-
+		// Declaração de variáveis
+		
+		// <Tipo> <nomeVariável> <atribuiçãoDeValorOpcional>
+		// Exemplos:
+		int idade;
+		int anoFabricacao = 2021;
+		double salarioMinimo = 2500.00;
+		
+		// Existem algumas peculiaridades ao se trabalhar com alguns tipos epecificos
+		byte idadee = 123;
+		short ano = 2021;
+		int cep = 21070333; // Se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // Se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+		
+		// Exemplo typecasting
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short)numeroNormal;
+		
+		int numero = 1;
+		numero = 2;
+	*/
+		
+		// Definir valores que nunca podera mudar - constantes / variáveis imutáveis
+		int numero = 5;
+		numero = 10;
+		System.out.println(numero);
+		
+		final double VALOR_DE_PI = 3.14;
+		
+		// Classe String
+		String meuNome = "Natanael Lima";
+		
 	}
 
 }
